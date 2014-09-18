780cd566b202811f3282c92e3103f39a4ca718e0
Avadhut Thakar <avdhut.thakar@gmail.com>
2014-09-17 17:36:11 -0700

    Lab2 Submission
    Lab2 Submission

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(connect.query()); // Parse query string into `request.query`
 
 app.use('/', main);
 
+
 function main(request, response, next) {
 	switch (request.method) {
 		case 'GET': get(request, response); break;
@@ -26,7 +27,7 @@ function get(request, response) {
 		var sid = cookies['session_id'];
 		if ( login.isLoggedIn(sid) ) {
 			response.setHeader('Set-Cookie', 'session_id=' + sid);
-			response.end(login.hello(sid));	
+			response.end(login.hello(sid));
 		} else {
 			response.end("Invalid session_id! Please login again\n");
 		}
@@ -34,31 +35,68 @@ function get(request, response) {
 		response.end("Please login via HTTP POST\n");
 	}
 };
-
 function post(request, response) {
-	// TODO: read 'name and email from the request.body'
-	// var newSessionId = login.login('xxx', 'xxx@gmail.com');
-	// TODO: set new session id to the 'session_id' cookie in the response
-	// replace "Logged In" response with response.end(login.hello(newSessionId));
+	/* Taking the request object into variable. Then we can copy all the required
+	 * values seperately.
+	 */
+	 var requestObject = request.body;
+	 var loggedInUserName = requestObject['name'];
+	 var loggedInUserEmail = requestObject['email'];
 
-	response.end("Logged In\n");
+	// var newSessionId = login.login('xxx', 'xxx@gmail.com');
+	// Form the sessionId using above given code
+	var newSessionId = login.login(loggedInUserName, loggedInUserEmail);
+	//Set session and content type.
+	response.setHeader('Set-Cookie', 'session_id=' + newSessionId);
+	response.setHeader('Content-Type', 'text/html');
+ 	response.end(login.hello(newSessionId));
+
 };
 
-function del(request, response) {
-	console.log("DELETE:: Logout from the server");
- 	// TODO: remove session id via login.logout(xxx)
- 	// No need to set session id in the response cookies since you just logged out!
 
-  	response.end('Logged out from the server\n');
+
+function del(request, response) {
+    console.log("DELETE:: Logout from the server");
+	var cookies = request.cookies;
+	console.log(cookies);
+ 	// No need to set session id in the response cookies since you just logged out!
+ 	// Validate passed out session is correct or not.
+	if ('session_id' in cookies) {
+		var sessionId = cookies['session_id'];
+		if ( login.isLoggedIn(sessionId) ) {
+			login.logout(sessionId);
+			response.end('Logged out from the server\n');
+		}else{
+			response.end("Invalid session_id! Please login again\n");
+		}
+	}else{
+  		response.end('No session id found in the request.\n');
+	}
 };
 
 function put(request, response) {
-	console.log("PUT:: Re-generate new seesion_id for the same user");
-	// TODO: refresh session id; similar to the post() function
 
-	response.end("Re-freshed session id\n");
+	console.log("PUT:: Re-generate new seesion_id for the same user");
+	// Read the cookies.
+	var cookies = request.cookies;
+	// Get the session id
+	var sessionId = cookies['session_id'];
+	//Cheking for correct session id
+	if ('session_id' in cookies){
+		if ( login.isLoggedIn(sessionId) ) {
+				// Form new session id
+				var newSessionId = login.login(login.sessionMap[sessionId].name, login.sessionMap[sessionId].email);
+       			// Set new session in the form of cookie
+       			response.setHeader('Set-Cookie', 'session_id=' + newSessionId);
+				response.end("Re-freshed session id\n");
+        	}else{
+	       		response.end("Invalid session_id! Please login again\n");
+		}
+	}else{
+		// Print = no session id found in the request
+		response.end("No Session ID found in the request\n");
+	}
 };
 
 app.listen(8000);
-
 console.log("Node.JS server running at 8000...");
--- login.js
+++ login.js
@@ -42,9 +42,8 @@ Login.prototype.login = function(_name, _email) {
  */ 
 Login.prototype.logout = function(sessionId) {
 	console.log('logout::' + sessionId);
-   /*
-	* TODO: Remove the given sessionId from the sessionMap
-	*/
+	delete this.sessionMap[sessionId];
+	console.log(sessionId)
 };
 
 // Export the Login class
